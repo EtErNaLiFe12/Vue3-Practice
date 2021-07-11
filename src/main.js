17f0a617a062b59db0ac7a1fe1639a80d2d5a73a
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-11 22:32:34 +0900

    button dataset.type 지정 및 클래스 및 인스턴스 생성  / input 태그에 연결

--- src/main.js
+++ src/main.js
@@ -2,3 +2,53 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const buttons = document.querySelectorAll('button');
+const displayElement = document.querySelector('input');
+
+class Calculator {
+  constructor(displayElement) { 
+      this.displayElement = displayElement
+      this.clear()
+  }    
+
+  appendNumber(number) {
+      this.displayContent += number
+  }
+
+  appendOperator(operator) {            
+      this.displayContent += operator
+  }
+
+  updateDisplay() {
+      this.displayElement.value = this.displayContent
+  }
+
+  clear() {
+      this.displayContent = ''
+      this.displayElement.value = 0
+  }
+}
+
+const calculator = new Calculator(displayElement)
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+      switch (button.dataset.type) {
+          case 'operator':
+              calculator.appendOperator(button.innerText)
+              calculator.updateDisplay()
+              break
+          case 'ac':
+              calculator.clear()
+              break
+          case 'equals':
+              console.log('equals')
+              break
+          default:
+              calculator.appendNumber(button.innerText)
+              calculator.updateDisplay()
+              break
+      }
+  })      
+})
