d0cbf945e1de5862bc5fdee5dafb5eaa37c76993
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-14 15:19:55 +0900

    enter 확인 기능 추가 및 구문별 일부 주석 추가

--- src/main.js
+++ src/main.js
@@ -9,18 +9,20 @@ const displayElement = document.querySelector('input');
 class Calculator {
   constructor(displayElement) { 
       this.displayElement = displayElement // 인수 초기화
-      this.operatorCheck = true // 연산자 중복 방지 확인 = false로 초기화
-      this.enterCheck = true
+      this.operatorCheck = true // 연산자 중복 방지 확인 = true로 초기화
+      this.enterCheck = true // enter 누름시 함수 빠져나가기 (아무런 값 없이 enter시)
       this.clear() // clear 메소드 실행
   }    
   appendNumber(number) {
       this.displayContent += number
       this.operatorCheck = false
+      this.enterCheck = false
   }
   subNumber() {
         // substring() 메소드를 통해 맨 첫번째 또는 맨 뒤의 값을 제거 해줌
        this.displayContent = this.displayElement.value.substring(0, this.displayElement.value.length-1)
        this.operatorCheck = false
+       this.enterCheck = false
   }
   appendOperator(operator) {
       if(this.operatorCheck) { // 만약 연산자가 true가 될경우에는 함수 빠져나가기
@@ -28,13 +30,13 @@ class Calculator {
       }
       this.displayContent += operator
       this.operatorCheck = true
-  }
-  entered() {
-    if(this.enterCheck)  {
-        this.displayContent = eval(this.displayContent)
-    }
+      this.enterCheck = false
   }
   compute() {
+    if(this.enterCheck) {
+        return
+    }
+    this.enterCheck = false
     this.displayContent = eval(this.displayContent)   // eval() 함수를 통해 계산 - 예) let a = 2 + 3 + 5; console.log(eval(a)); = 9 / 포함되어있는 문자들을 계산하는 함수
     }
   updateDisplay() {
@@ -44,13 +46,17 @@ class Calculator {
       // reset시 또는 초기상태
       this.displayContent = '' 
       this.displayElement.value = '0'
+      this.enterCheck = true
   }
 }
 
+// 객체 생성 및 인스턴스 생성
 const calculator = new Calculator(displayElement)
 
+//forEach문으로 각 버튼 연결
 buttons.forEach(button => {
   button.addEventListener('click', () => {
+      //switch문을 통해 button에 지정한 dataset.type 값으로 구분하여 메소드 실행 
       switch (button.dataset.type) {
           case 'operator':
               calculator.appendOperator(button.innerText)
@@ -64,7 +70,6 @@ buttons.forEach(button => {
               calculator.clear()
               break
           case 'enter':
-              calculator.entered()
               calculator.compute()
               calculator.updateDisplay()
               break
