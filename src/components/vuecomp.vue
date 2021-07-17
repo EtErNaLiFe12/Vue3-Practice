a39638ff28dbc8542c6f48b25421a7eca8db6199
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-17 14:24:23 +0900

    component 기본 내용 작성

---
+++ src/components/vuecomp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="piano">
+    <div :[attr]="'pianos'"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      attr: 'class'
+      }
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
