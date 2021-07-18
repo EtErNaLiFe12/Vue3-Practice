2e31fe5d74fd716e76d07c50f27a39258a3a433d
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-07-18 16:07:00 +0900

    v-if / 클릭이벤트 여러개 동작

--- src/components/vuecomp.vue
+++ src/components/vuecomp.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="piano">
-    <div :[attr]="'pianos'"></div>
+  <div
+    class="comp-container"
+    @[evt]="addEl(), seenEl()"
+    :class="{cc : colorChg}">
+    <div
+      class="sub-container"
+      @[evt]="addEl"
+      :class="{cc : colorChg}">
+      <ul>
+        <li v-if="seen">
+          1
+        </li>
+        <li v-if="seen">
+          2
+        </li>
+        <li v-if="seen">
+          3
+        </li>
+        <li v-if="seen">
+          4
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,15 +29,66 @@
 export default {
   data() {
     return {
-      attr: 'class'
+      colorChg: false,
+      evt: 'click',
+      seen: true
       }
+  },
+  methods: {
+    addEl() {
+      this.colorChg = !this.colorChg
+    },
+    seenEl() {
+      this.seen = !this.seen
+    }
   }
-  
 }
 </script>
 
 <style lang="scss" scoped>
 
+$col1: #fb5849;
+$col2: #696969;
+
+  .comp-container {
+    position: relative;
+    width: 1500px;
+    height: 150px;
+    margin: 50px auto;
+    border: 1px solid red;
+    
+    .sub-container {
+      position: absolute;
+      width: 800px;
+      height: 100px;
+      top: 0;
+      bottom:0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      
+      ul {
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 3px solid $col2;
+        
+        li {
+          margin: 10px;
+          font-size: 30px;
+          font-weight: 700;
+          color: $col1;
+        }
+
+      }
+
+    }
+
+  }
+  .cc {
+    background-color: $col1;
+  }
 
 
 </style>
